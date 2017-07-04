b358d2f65634cce6a1f13df11feb9704f528674e
cdiazp01 <cdiazp01@LKOQCAP>
2017-07-04 09:28:40 -0400

    - Tunning performance of symfony in dev

--- app/autoload.php
+++ app/autoload.php
@@ -4,7 +4,9 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
 /** @var ClassLoader $loader */
-$loader = require __DIR__.'/../vendor/autoload.php';
+// $loader = require __DIR__.'/../vendor/autoload.php';
+$loader = require '/app-vendor/autoload.php';
+
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
--- web/app_dev.php
+++ web/app_dev.php
@@ -12,10 +12,10 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')) || PHP_SAPI === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '172.27.0.1')) || PHP_SAPI === 'cli-server')
 ) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+     header('HTTP/1.0 403 Forbidden');
+     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
 /** @var \Composer\Autoload\ClassLoader $loader */
